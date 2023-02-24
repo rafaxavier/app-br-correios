1cde7540fd3db4293b36e2a33079cf6441846557
rafael <rafaworksi@outlook.com>
2023-02-24 00:16:38 -0300

    feat: create CalcScreen

---
+++ src/screens/CalcScreen/styles.js
@@ -0,0 +1,16 @@
+import { View, StyleSheet, Image, Text, TextInput, TouchableOpacity, SafeAreaView } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    // flexDirection:'column',  (default no react-native)
+    backgroundColor: '#F9F905',
+    // alinha verticalmente
+    justifyContent: 'space-around',
+    // alinha horizontalmente
+    alignItems: 'center',
+      // padding: 15,
+    width: '100%',
+  },
+
+});
