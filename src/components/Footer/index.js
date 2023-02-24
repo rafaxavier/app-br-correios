9afe2f362de87b9310f275b41d17d10e999565bb
rafael <rafaworksi@outlook.com>
2023-02-24 00:15:52 -0300

    feat: create footer navbar component

---
+++ src/components/Footer/index.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { View, TouchableOpacity } from 'react-native';
+import { styles } from './styles';
+import IconMaterial from 'react-native-vector-icons/MaterialIcons';
+import IconMaterialC from 'react-native-vector-icons/MaterialCommunityIcons';
+export default function Footer({ navigation }) {
+
+  async function navigationHome() {
+    navigation.navigate('HomeScreen');
+  }
+
+  async function navigationCalcularFrete() {
+    navigation.navigate('CalcScreen');
+  }
+
+  async function navigationGetCep() {
+    navigation.navigate('GetCepScreen');
+  }
+
+  return (
+    <View style={styles.footer} >
+      <TouchableOpacity onPress={navigationHome}>
+        <IconMaterialC style={styles.icon} name="package-variant-closed" />
+      </TouchableOpacity>
+      <TouchableOpacity onPress={navigationCalcularFrete}>
+        <IconMaterialC style={styles.icon} name="calculator-variant-outline" />
+      </TouchableOpacity>
+      <TouchableOpacity onPress={navigationGetCep}>
+        <IconMaterialC style={styles.icon} name="map-search-outline" />
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+
+
+
---
+++ src/components/Footer/styles.js
@@ -0,0 +1,23 @@
+import { View, StyleSheet, Image, Text, TextInput, TouchableOpacity, SafeAreaView } from 'react-native';
+
+export const styles = StyleSheet.create({
+  footer: {
+    // flex: 1,
+    justifyContent: 'space-around',
+    position:'absolute',
+    bottom:0,
+    backgroundColor: '#153FA7',
+    height: 90,
+    width: '100%',
+    flexDirection:'row',
+    // borderTopStartRadius:15,
+    // borderTopEndRadius:15
+  },
+  icon: {
+    top:15,
+    color: '#fff',
+    fontSize:60, 
+  },
+
+
+});
