f67a8596cb3604d34a8787154a4f6dbbcf436e7d
rafaxavier <rxn.90s@gmail.com>
2023-02-24 01:40:08 -0300

    feat: create service getCep

--- src/screens/GetCepScreen/index.js
+++ src/screens/GetCepScreen/index.js
@@ -2,22 +2,68 @@ import React, { useState, useEffect } from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { View, Image, Text, TextInput, TouchableOpacity, SafeAreaView } from 'react-native';
 import { styles } from './styles';
-import IconMaterial from 'react-native-vector-icons/MaterialIcons';
+import IconFontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 import IconMaterialC from 'react-native-vector-icons/MaterialCommunityIcons';
 import Footer from '../../components/Footer';
-
+import api from './../../services/api';
 
 export default function GetCepScreen({ navigation }) {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [cep, setCep] = useState('');
   const [error, setError] = useState('');
+  const [data, setData] = useState('');
+
+  async function handleGetCep() {
+    try {
+      const response = await api.get(`/busca-cep?cep=${cep}`);
+      setData(response.data);
+
+    } catch (error) {
+      setError(error.response.data.msg);
+    }
+
+  }
 
   return (
     <SafeAreaView style={styles.container} >
-      <View>
-        <Text>Buscar por cep</Text>
+      <View style={styles.top}>
+        <Text style={styles.title}>Buscar por cep</Text>
+        <TextInput style={styles.input}
+          keyboardType="email-address"
+          autoCapitalize="none"
+          autoCorrect={false}
+          placeholder="Digite o cep"
+          placeholderTextColor="#153CA7"
+          value={cep}
+          onChangeText={setCep}
+        />
+        <TouchableOpacity onPress={handleGetCep}>
+          <IconFontAwesome5 style={styles.icon} name="search-location" />
+        </TouchableOpacity>
+        {
+          // caso não tenha dog
+          data 
+            ?
+            <View >
+              <Text>Cep: {data.cep}</Text>
+              <Text>Logradouro: { data.logradouro }</Text>
+              <Text>Complemento: { data.complemento }</Text>
+              <Text>Bairro: { data.bairro }</Text>
+              <Text>Localidade: { data.localidade }</Text>
+              <Text>UF: { data.uf }</Text>
+              <Text>ibge: { data.ibge }</Text>
+              <Text>siafi: { data.siafi }</Text>
+              <Text>DDD: { data.ddd }</Text>
+            </View>
+
+            :
+            <View>
+              <Text> Nenhum cep</Text>
+            </View>
+
+        }
+
       </View>
-      <Footer navigation={navigation}/>
+      <Footer navigation={navigation} />
     </SafeAreaView>
   );
 }
--- src/screens/GetCepScreen/styles.js
+++ src/screens/GetCepScreen/styles.js
@@ -6,11 +6,39 @@ export const styles = StyleSheet.create({
     // flexDirection:'column',  (default no react-native)
     backgroundColor: '#F9F905',
     // alinha verticalmente
-    justifyContent: 'space-around',
+    justifyContent: 'center',
     // alinha horizontalmente
     alignItems: 'center',
       // padding: 15,
-    width: '100%',
+    // width: '100%',
+  },
+  top:{
+    position:'absolute',
+    top:40,
   },
 
+  title:{
+    fontSize:35, 
+    color: '#153CA7',
+    alignSelf:'center',
+    top:0
+  },
+
+  input: {
+    width: 290,
+    height: 50,
+    backgroundColor: '#fff',
+    borderWidth: 3,
+    borderColor: '#153CA7',
+    borderRadius: 12,
+    paddingHorizontal: 15,
+    fontSize:19
+  },
+  icon: {
+    position: 'absolute',
+    bottom:5,
+    color: '#153CA7',
+    fontSize:35, 
+    right:15
+  },
 });
