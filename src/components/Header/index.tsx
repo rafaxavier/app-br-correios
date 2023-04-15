5a85fde6976796ad0896a919d96f75b7c2d295a9
rafaxavier <rxn.90s@gmail.com>
2023-04-14 21:40:17 -0300

    feat: create header components

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,33 @@
+import React, { ReactNode } from 'react';
+import { Text, TouchableOpacity, View } from 'react-native';
+import { styles } from './styles';
+
+interface HeaderProps {
+  title: string;
+  leftIcon: {
+    iconComponent: ReactNode;
+    onPress: () => void;
+  };
+  rightIcon: {
+    iconComponent: ReactNode;
+    onPress: () => void;
+  };
+}
+
+export default function Header({ title, leftIcon, rightIcon }: HeaderProps) {
+  return (
+    <View style={styles.topo}>
+      {leftIcon && (
+        <TouchableOpacity onPress={leftIcon.onPress}>
+          {leftIcon.iconComponent}
+        </TouchableOpacity>
+      )}
+      <Text style={styles.title}>{title}</Text>
+      {rightIcon && (
+        <TouchableOpacity onPress={rightIcon.onPress}>
+          {rightIcon.iconComponent}
+        </TouchableOpacity>
+      )}
+    </View>
+  );
+}
---
+++ src/components/Header/styles.tsx
@@ -0,0 +1,19 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  topo: {
+    position: 'absolute',
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
+    top: 0,
+    left: 0,
+    right: 0,
+    margin: 5,
+  },
+  title: {
+    fontSize: 25,
+    color: '#FFF',
+    fontWeight: '600',
+  },
+});
