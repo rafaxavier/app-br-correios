5ebf16601e132502b2e414df6a735f34533ec217
rafaxavier <rxn.90s@gmail.com>
2023-04-14 22:18:57 -0300

    feat: finalized homeScreen

--- src/screens/HomeScreen/index.tsx
+++ src/screens/HomeScreen/index.tsx
@@ -1,25 +1,19 @@
-import React, { useState, useEffect, useRef } from 'react';
-import {
-  View,
-  Text,
-  TouchableOpacity,
-  SafeAreaView,
-  ScrollView,
-  Modal,
-} from 'react-native';
-import IconFontAwesome5 from 'react-native-vector-icons/FontAwesome5';
-import IconMaterial from 'react-native-vector-icons/MaterialIcons';
+import React, { useState, useEffect } from 'react';
+import { View, SafeAreaView, Modal } from 'react-native';
+import IconMaterialCI from 'react-native-vector-icons/MaterialCommunityIcons';
 import { LinearGradient } from 'expo-linear-gradient';
 import { StackNavigationProp } from '@react-navigation/stack';
+import Header from '../../components/Header';
 import Footer from '../../components/Footer';
 import { styles } from './styles';
 import AddCodRastreioModal from './ModalAddCodigo';
+import ModalDetalhesObjeto from './ModalDetalhes';
 import {
   getItems,
   deletarObjetoById,
   fecthObjeto,
-  deletarTodosObjetos,
 } from '../../services/storage';
+import RenderList from './RenderList';
 
 type RootStackParamList = {
   HomeScreen: undefined;
@@ -33,33 +27,34 @@ interface FooterProps {
   navigation: NavigationProps;
 }
 
+interface Objeto {
+  id: string;
+  eventos: Evento[];
+}
+
+interface Evento {
+  descricao: string;
+  codigo: string;
+  tipo: string;
+  dtHrCriado: string;
+}
+
 export default function HomeScreen({ navigation }: FooterProps): JSX.Element {
-  const [error, setError] = useState<string>('');
-  const [data, setData] = useState<any[]>([]);
-  const [modalVisible, setModalVisible] = useState<boolean>(false);
+  const [data, setData] = useState<Objeto[]>([]);
+  const [modalAddObjeto, setModalAddObjeto] = useState<boolean>(false);
+  const [modalDetalhesObjeto, setModalDetalhesObjeto] =
+    useState<boolean>(false);
   const [cod, setCode] = useState<string>('');
+  const [objeto, setObjeto] = useState<Objeto>();
 
   async function loadItems() {
-    // *******************************
-    // deletarTodosObjetos();
-
     const dados = await getItems();
-
-    // dados.map(val => {
-    //   console.log(val.id);
-    //   console.log(val.eventos[0]);
-    //   // eslint-disable-next-line array-callback-return
-    //   // val.eventos.map(evento => {
-    //   //   console.log(evento.descricao);
-    //   // });
-    // });
-
     setData(dados.filter(val => val && val.eventos));
-    // setData(dados);
   }
 
   async function handleDeletarObjetoById(id: string): Promise<void> {
     await deletarObjetoById(id);
+    setModalDetalhesObjeto(false);
     loadItems();
   }
 
@@ -67,8 +62,6 @@ export default function HomeScreen({ navigation }: FooterProps): JSX.Element {
     if (cod) {
       fecthObjeto(cod).then(() => loadItems());
     }
-
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [cod]);
 
   useEffect(() => {
@@ -80,17 +73,20 @@ export default function HomeScreen({ navigation }: FooterProps): JSX.Element {
     }
 
     fetchAndUpdateObjects();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   useEffect(() => {
     loadItems();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   function handleModalAddCod(): void {
-    setModalVisible(true);
+    setModalAddObjeto(true);
   }
+
+  const handleModalDetalhesObjeto = (val: Objeto): void => {
+    setObjeto(val);
+    setModalDetalhesObjeto(true);
+  };
 
   const onModalInputChange = (value: string) => {
     setCode(value);
@@ -102,93 +98,51 @@ export default function HomeScreen({ navigation }: FooterProps): JSX.Element {
         colors={['#153CA7', '#F9F905']}
         style={styles.background}
       />
-      <Modal transparent animationType="slide" visible={modalVisible}>
+      {/* modal add objeto */}
+      <Modal transparent animationType="slide" visible={modalAddObjeto}>
         <AddCodRastreioModal
           onValue={onModalInputChange}
-          closeModal={() => setModalVisible(false)}
+          closeModal={() => setModalAddObjeto(false)}
         />
       </Modal>
 
+      {/* modal exibe detalhes objeto */}
+      <Modal transparent animationType="slide" visible={modalDetalhesObjeto}>
+        {objeto && (
+          <ModalDetalhesObjeto
+            obj={objeto}
+            closeModal={() => setModalDetalhesObjeto(false)}
+            handleDeletarObjetoById={() => handleDeletarObjetoById(objeto.id)}
+          />
+        )}
+      </Modal>
+
       {/* header */}
-      <View style={styles.topo}>
-        <Text style={styles.title}>Rastreio de Objetos</Text>
-        <TouchableOpacity onPress={() => handleModalAddCod()}>
-          <IconFontAwesome5 style={styles.iconAdd} name="plus-circle" />
-        </TouchableOpacity>
+      <View style={{ marginTop: 25, marginBottom: 15 }}>
+        <Header
+          title="Rastreio de Objetos"
+          rightIcon={{
+            iconComponent: (
+              <IconMaterialCI
+                style={{ fontSize: 45, color: '#F9F905' }}
+                name="plus-circle"
+              />
+            ),
+            onPress: handleModalAddCod,
+          }}
+          leftIcon={{
+            iconComponent: undefined,
+            onPress(): void {
+              throw new Error('Function not implemented.');
+            },
+          }}
+        />
       </View>
-      <ScrollView style={styles.cardContainer}>
-        {data.length > 0
-          ? data.map(val => {
-              // console.log(val.id);
-              // console.log(val.eventos[0]);
-              return (
-                <TouchableOpacity key={val.id} style={styles.card}>
-                  <View style={styles.line1}>
-                    {val.eventos[0].codigo === 'RO' && (
-                      <IconMaterial
-                        style={styles.iconCaminho}
-                        name="local-shipping"
-                      />
-                    )}
-                    {val.eventos[0].codigo === 'BDE' &&
-                      val.eventos[0].tipo === '01' && (
-                        <IconFontAwesome5
-                          style={styles.iconSuccess}
-                          name="check-circle"
-                        />
-                      )}
-                    {val.eventos[0].codigo === 'OEC' &&
-                      val.eventos[0].tipo === '01' && (
-                        <IconFontAwesome5
-                          style={styles.iconAlert}
-                          name="info-circle"
-                        />
-                      )}
-                    {val.eventos[0].codigo === 'BDE' &&
-                      val.eventos[0].tipo === '20' && (
-                        <IconFontAwesome5 style={styles.iconSad} name="tired" />
-                      )}
-                    {val.eventos[0].codigo === 'PAR' &&
-                      val.eventos[0].tipo === '10' && (
-                        <IconMaterial
-                          style={styles.iconFisc}
-                          name="local-police"
-                        />
-                      )}
-                    {val.eventos[0].codigo === 'PAR' &&
-                      val.eventos[0].tipo === '16' && (
-                        <IconFontAwesome5
-                          style={styles.iconChecking}
-                          name="map-marker-alt"
-                        />
-                      )}
-                    {val.eventos[0].codigo === 'PO' &&
-                      val.eventos[0].tipo === '01' && (
-                        <IconMaterial
-                          style={styles.iconPost}
-                          name="airplanemode-on"
-                        />
-                      )}
-                    <Text style={styles.descricao} numberOfLines={1}>
-                      {val.eventos[0].descricao}
-                    </Text>
-                  </View>
-                  <View style={styles.line2}>
-                    <Text style={styles.codigo}>{val.id}</Text>
-                    <Text style={styles.data}>{val.eventos[0].dtHrCriado}</Text>
-                    <TouchableOpacity>
-                      <IconMaterial
-                        style={styles.iconDeletePost}
-                        name="delete-sweep"
-                        onPress={() => handleDeletarObjetoById(val.id)}
-                      />
-                    </TouchableOpacity>
-                  </View>
-                </TouchableOpacity>
-              );
-            })
-          : null}
-      </ScrollView>
+
+      <RenderList
+        dados={data}
+        handleModalDetalhesObjeto={handleModalDetalhesObjeto}
+      />
       <Footer navigation={navigation} />
     </SafeAreaView>
   );
