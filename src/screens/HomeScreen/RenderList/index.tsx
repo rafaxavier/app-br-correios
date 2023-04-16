33174260f9b5ad9dea104c757a4216e06cea7de8
rafaxavier <rxn.90s@gmail.com>
2023-04-15 23:26:35 -0300

    feat: create renderList component

---
+++ src/screens/HomeScreen/RenderList/index.tsx
@@ -0,0 +1,138 @@
+import React from 'react';
+import { View, Text, TouchableOpacity, ScrollView } from 'react-native';
+import IconFontAwesome5 from 'react-native-vector-icons/FontAwesome5';
+import { convertDate } from '../../../services/utils';
+import { styles } from './styles';
+
+interface Props {
+  handleModalDetalhesObjeto?: (value: Objeto) => void;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  dados?: any[];
+}
+
+interface Objeto {
+  id: string;
+  eventos: Evento[];
+}
+
+interface Evento {
+  descricao: string;
+  evento: string;
+  codigo: string;
+  tipo: string;
+  dtHrCriado: string;
+}
+
+export default function RenderList({
+  handleModalDetalhesObjeto,
+  dados = [],
+}: Props) {
+  return (
+    <ScrollView style={styles.cardContainer}>
+      {dados.map((value, index) => {
+        const event: Evento | null = value.eventos ? value.eventos[0] : null;
+        const codigo = event ? event.codigo : value.codigo;
+        const tipo = event ? event.tipo : value.tipo;
+        const descricao = event ? event.descricao : value.descricao;
+        const dtHrCriado = event ? event.dtHrCriado : value.dtHrCriado;
+        const getIcon = () => {
+          switch (codigo) {
+            case 'RO':
+              return 'truck';
+            case 'BDE':
+              if (tipo === '01') {
+                return 'check-circle';
+              }
+              if (tipo === '20') {
+                return 'minus-circle';
+              }
+              return null;
+
+            case 'OEC':
+              if (tipo === '01') {
+                return 'info-circle';
+              }
+              return null;
+
+            case 'PAR':
+              if (tipo === '10') {
+                return 'cubes';
+              }
+              if (tipo === '16') {
+                return 'map-marker-alt';
+              }
+              return null;
+
+            case 'PO':
+              if (tipo === '01') {
+                return 'plane';
+              }
+              return null;
+
+            default:
+              return null;
+          }
+        };
+
+        const icon = getIcon();
+
+        const iconStyle = (() => {
+          switch (icon) {
+            case 'truck':
+              return styles.iconCaminho;
+            case 'check-circle':
+              return styles.iconSuccess;
+            case 'info-circle':
+              return styles.iconAlert;
+            case 'minus-circle':
+              return styles.iconSad;
+            case 'cubes':
+              return styles.iconFisc;
+            case 'map-marker-alt':
+              return styles.iconChecking;
+            case 'plane':
+              return styles.iconPost;
+            default:
+              return null;
+          }
+        })();
+
+        return event ? (
+          <TouchableOpacity
+            key={value.id || `${index}`}
+            style={styles.card}
+            onPress={() => {
+              if (handleModalDetalhesObjeto) {
+                handleModalDetalhesObjeto(value);
+              }
+            }}
+          >
+            <View style={styles.line1}>
+              {icon && <IconFontAwesome5 style={iconStyle} name={icon} />}
+              <Text style={styles.descricao} numberOfLines={1}>
+                {descricao}
+              </Text>
+            </View>
+            <View style={styles.line2}>
+              <Text style={styles.codigo}>{value.id}</Text>
+              <Text style={styles.data}>{convertDate(dtHrCriado)}</Text>
+            </View>
+          </TouchableOpacity>
+        ) : (
+          <View style={styles.card2} key={value.id || `${index}`}>
+            <View style={styles.line1}>
+              {icon && <IconFontAwesome5 style={iconStyle} name={icon} />}
+              <Text style={{ fontSize: 14.5, marginLeft: 2 }}>{descricao}</Text>
+            </View>
+            <Text>{dtHrCriado}</Text>
+          </View>
+        );
+      })}
+    </ScrollView>
+  );
+}
+
+RenderList.defaultProps = {
+  handleModalDetalhesObjeto: undefined,
+  dados: [],
+};
---
+++ src/screens/HomeScreen/RenderList/styles.tsx
@@ -0,0 +1,115 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  background: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+  },
+
+  cardContainer: {
+    width: '100%',
+    top: 40,
+    marginBottom: 50,
+  },
+  card: {
+    height: 70,
+    marginHorizontal: 10,
+    marginVertical: 5,
+    padding: 5,
+    backgroundColor: '#fff',
+    borderRadius: 12,
+  },
+  card2: {
+    height: 70,
+    padding: 9,
+    margin: 4,
+    backgroundColor: '#fff',
+    borderRadius: 10,
+    paddingBottom: 10,
+  },
+
+  line1: {
+    maxWidth: 315,
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+  },
+  line2: {
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+  },
+
+  // formatacao icones de status
+  iconSuccess: {
+    width: 40,
+    fontSize: 36,
+    color: 'green',
+  },
+
+  iconCaminho: {
+    width: 40,
+    fontSize: 30,
+    color: 'purple',
+  },
+
+  iconAlert: {
+    width: 40,
+    fontSize: 34,
+    color: 'orange',
+  },
+
+  iconSad: {
+    width: 40,
+    fontSize: 35,
+    color: 'red',
+  },
+
+  iconFisc: {
+    width: 40,
+    fontSize: 34,
+    color: 'blue',
+  },
+
+  iconChecking: {
+    width: 35,
+    fontSize: 34,
+    color: 'brown',
+  },
+  iconPost: {
+    width: 40,
+    fontSize: 35,
+    color: '#326735',
+  },
+
+  descricao: {
+    maxWidth: 300,
+    marginTop: 8,
+    fontWeight: 'bold',
+    fontSize: 16,
+    color: '#153CA7',
+  },
+  codigo: {
+    fontSize: 14,
+    color: 'grey',
+    fontWeight: '500',
+  },
+  data: {
+    fontSize: 14,
+    color: 'grey',
+    fontWeight: '500',
+  },
+
+  iconDeletePost: {
+    width: 40,
+    fontSize: 35,
+    color: '#A90000',
+  },
+});
