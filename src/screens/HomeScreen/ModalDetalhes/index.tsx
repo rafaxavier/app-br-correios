dfc4634b74784e45b4496254bb38c5a16684cb70
rafaxavier <rxn.90s@gmail.com>
2023-04-15 23:25:35 -0300

    feat: create modalDetalhes component

---
+++ src/screens/HomeScreen/ModalDetalhes/index.tsx
@@ -0,0 +1,69 @@
+import React, { useState } from 'react';
+import { View, Text } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+import IconMaterialCI from 'react-native-vector-icons/MaterialCommunityIcons';
+import Header from '../../../components/Header';
+import { styles } from './styles';
+import RenderList from '../RenderList';
+
+interface Props {
+  closeModal: () => void;
+  handleDeletarObjetoById: (id: string) => Promise<void>;
+  obj: Objeto;
+}
+
+interface Objeto {
+  id: string;
+  eventos: Evento[];
+}
+
+interface Evento {
+  descricao: string;
+  evento: string;
+  codigo: string;
+  tipo: string;
+  dtHrCriado: string;
+}
+
+export default function ModalDetalhesObjeto({
+  closeModal,
+  handleDeletarObjetoById,
+  obj,
+}: Props) {
+  const [objeto] = useState<Objeto>(obj);
+
+  return (
+    <View style={styles.container}>
+      <LinearGradient
+        colors={['#153CA7', '#F9F905']}
+        style={styles.background}
+      />
+      <View>
+        <Header
+          title="Status do Objeto"
+          leftIcon={{
+            iconComponent: (
+              <IconMaterialCI
+                style={{ fontSize: 45, color: '#F9F905' }}
+                name="arrow-left-circle"
+              />
+            ),
+            onPress: closeModal,
+          }}
+          rightIcon={{
+            iconComponent: (
+              <IconMaterialCI
+                style={{ fontSize: 45, color: 'red' }}
+                name="delete-circle"
+              />
+            ),
+            onPress: () => handleDeletarObjetoById(objeto.id),
+          }}
+        />
+        <Text style={styles.codigoHeader}>{objeto.id}</Text>
+
+        <RenderList dados={objeto.eventos} />
+      </View>
+    </View>
+  );
+}
---
+++ src/screens/HomeScreen/ModalDetalhes/styles.tsx
@@ -0,0 +1,28 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  background: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+  },
+
+  // modal: {
+  //   // alignItems: 'center',
+  //   paddingBottom: -60,
+  // },
+
+  codigoHeader: {
+    top: 40,
+    alignSelf: 'center',
+    color: '#fff',
+  },
+});
