2a17843340ecc5a2411ea26fe5d85ff02e8ddf63
rafaxavier <rxn.90s@gmail.com>
2023-04-20 00:09:25 -0300

    feat: include type navigation CalcScreen

--- src/screens/CalcScreen/index.tsx
+++ src/screens/CalcScreen/index.tsx
@@ -1,18 +1,29 @@
-import React, { useState, useEffect } from 'react';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import {
-  View, Image, Text, TextInput, TouchableOpacity, SafeAreaView,
-} from 'react-native';
-// import IconMaterial from 'react-native-vector-icons/MaterialIcons';
-// import IconMaterialC from 'react-native-vector-icons/MaterialCommunityIcons';
+import React from 'react';
+import { View, Text, SafeAreaView } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
+import { StackNavigationProp } from '@react-navigation/stack';
 import Footer from '../../components/Footer';
 import { styles } from './styles';
 
-export default function CalcScreen({ navigation}) {
+type RootStackParamList = {
+  HomeScreen: undefined;
+  CalcScreen: undefined;
+  GetCepScreen: undefined;
+};
+
+type NavigationProps = StackNavigationProp<RootStackParamList>;
+
+interface CalcScreenProps {
+  navigation: NavigationProps;
+}
+
+export default function CalcScreen({ navigation }: CalcScreenProps) {
   return (
     <SafeAreaView style={styles.container}>
-      <LinearGradient colors={['#153CA7', '#F9F905']} style={styles.background} />
+      <LinearGradient
+        colors={['#153CA7', '#F9F905']}
+        style={styles.background}
+      />
       <View>
         <Text style={styles.title}>Calcular frete</Text>
       </View>
