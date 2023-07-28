b79b31b925e2ea7cf20492b51bbc5ceac1e07c48
rafaxavier <rxn.90s@gmail.com>
2023-07-28 00:29:58 -0300

    feat: create LojasScreen

---
+++ src/screens/LojasScreen/styles.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  background: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+  },
+});
